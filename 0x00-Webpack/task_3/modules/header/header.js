0c82af9e56312b2ce98555c0d1d5eead5d9217c2
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-08-24 04:01:14 +0100

    header module

--- 0x00-Webpack/task_3/modules/header/header.js
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -4,4 +4,4 @@ const $ = require("jquery");
 $("body").append("<header></header>");
 $("header").append('<div id="logo"></div>', "<h1>Holberton Dashboard</h1>");
 
-console.log("Init header");``
+console.log("Init header");
